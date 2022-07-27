7d4d17e831c4c5eec7a220e30127fef804f548b1
Mukund <mukki11@Mukunds-MacBook-Air.local>
2022-07-27 22:54:27 +0530

    Selection Sort

--- sorting.c
+++ sorting.c
@@ -14,6 +14,20 @@ void insertionSort(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+void selectionSort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
 
 int main()
 {
