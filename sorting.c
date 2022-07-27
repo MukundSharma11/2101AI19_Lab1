13889c66a1f6456e5b5c28a480bac4edeb2bad52
Mukund <mukki11@Mukunds-MacBook-Air.local>
2022-07-27 22:57:33 +0530

    quick sort

--- sorting.c
+++ sorting.c
@@ -91,7 +91,40 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 }
+void quickSort(int arr[], int l, int r)
+{
+    if (l < r)
+    {
+        int q = partition(arr, l, r);
+        quickSort(arr, l, q - 1);
+        quickSort(arr, q + 1, r);
+    }
+}
 
+int partition(int arr[], int l, int r)
+{
+    int i = l - 1;
+    int j = r;
+    int pivot = arr[r];
+    while (1)
+    {
+        while (arr[++i] < pivot);
+        while (arr[--j] > pivot);
+        if (i < j)
+            swap(&arr[i], &arr[j]);
+        else
+            break;
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
     printf("Choose one options from the following:\n");
