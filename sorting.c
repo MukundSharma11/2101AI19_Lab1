d5d0573442f9b27fd2064090c7ec5804825d222c
Mukund <mukki11@Mukunds-MacBook-Air.local>
2022-07-27 22:55:14 +0530

    Bubble sort

--- sorting.c
+++ sorting.c
@@ -28,6 +28,18 @@ void selectionSort(int arr[], int n)
         arr[i] = temp;
     }
 }
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+}
 
 int main()
 {
